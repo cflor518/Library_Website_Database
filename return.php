11c677052e0d7c66b2fea7897d2ecca20968489a
andreapadilla <andpadi93@gmail.com>
2018-11-15 17:11:52 -0800

    Create return.php

---
+++ return.php
@@ -0,0 +1,5 @@
+In return <br><br>
+Testing <br><br>
+<a href="loggedin.php">Back</a>
+
+<!-- This file will allow people to return books that they have checked out. It will do this by deleting the books from the reserves table. -->
