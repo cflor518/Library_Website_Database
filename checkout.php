3276c1b53661133d82be66fa312530c16f354809
andreapadilla <andpadi93@gmail.com>
2018-11-15 17:13:10 -0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,3 +1,7 @@
 In checkout <br><br>
 Testing <br><br>
 <a href="loggedin.php">Back</a>
+
+<!-- this file is going to show people what books have not been checked out, 
+and allow them to check out one of the books, it will do this by addign the 
+ISBN of the book and the libID of the user into the reserves table. -->
