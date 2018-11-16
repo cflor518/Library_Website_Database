6ae3632083cd6f6d9a4a10bfb0342cbc29e83247
andreapadilla <andpadi93@gmail.com>
2018-11-15 17:07:04 -0800

    changed location for if login successful

--- authen_login.php
+++ authen_login.php
@@ -14,27 +14,20 @@ if (isset($_POST['fname']) and isset($_POST['lname']) and isset($_POST['libID'])
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
 	$libID = $_POST['libID'];
-
   	$_SESSION['fname'] = $fname;
   	$_SESSION['lname'] = $lname;
   	$_SESSION['libID'] = $libID;
-
-
 	// CHECK FOR THE RECORD FROM TABLE
 	$query = "SELECT * FROM `people` WHERE fname='$fname' and lname= '$lname' and libID='$libID'";
  
 	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
 	$count = mysqli_num_rows($result);
-
 	if ($count == 1){
-		echo "Login Credentials verified";
-		header('Location: bookcheckout.php');
-		//^^wherever it should go when it authenticates
+		header('Location: loggedin.php');
 	}
 	else {
-		//invalid login so sent back to welcome screen
-		//echo "Invalid Login Credentials";
-		header('Location: libwelcome.php');
+		//invalid login so sent to screen that prompts them to try again or go back to home
+		header('Location: login_inco.php');
 		exit;
 	}
 }
