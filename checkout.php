c8009f40e75655a97fa00e2ea90bf40c4dec6465
andreapadilla <andpadi93@gmail.com>
2018-11-15 21:30:31 -0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,6 +1,33 @@
-In checkout <br><br>
-Testing <br><br>
-<a href="loggedin.php">Back</a>
+<h3>Books Available for checkout:</h3>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "Library";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM books WHERE ISBN NOT IN (SELECT ISBN FROM reserves);";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "TITLE: " . $row["title"]. " <br> LAST NAME: " . $row["lauthor"]. "<br><br>";
+    }
+} 
+else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+<h3>What book would you like to check out?</h3>
 
 <!-- this file is going to show people what books have not been checked out, 
 and allow them to check out one of the books, it will do this by addign the 
