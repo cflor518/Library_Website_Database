0ef57ef56d7fa33b818a35bf269d846b43fdc768
Cesar Flores <cflor518@gmail.com>
2018-12-04 14:41:27 -0800

    Changed name of isbn to ISBN and libid and libID
    For compatibility issues

--- loggedin.php
+++ loggedin.php
@@ -66,19 +66,19 @@ if ($conn->connect_error) {
 }
 //Sessions variable assigned during authen_login.php
 $currentUserID = $_SESSION["libID"];
-$sql = "SELECT * FROM reserves where libid = " . $currentUserID ;
+$sql = "SELECT * FROM reserves where libID = " . $currentUserID ;
 $result = $conn->query($sql);
 	$count = 0;
 	echo "<form action=\"\" method=\"post\" action=\"loggedin.php\">";
         //For every entry in the reserves table for this user, we dynamically write a table
         //entry for the row on the page in HTML by echoing HTML syntax in php.
 	while($row = $result->fetch_assoc()){
-	  $returnUsersBooksSQL = "SELECT * FROM books where isbn = " . $row["isbn"];
+	  $returnUsersBooksSQL = "SELECT * FROM books where ISBN = " . $row["ISBN"];
 	  $returnUsersResult = $conn->query($returnUsersBooksSQL);
 	  $booksrow = $returnUsersResult->fetch_assoc();
 	  echo "<tr><td>". $booksrow["title"] . "</td>";
 	  echo "<td>". $booksrow["fauthor"] . " " . $booksrow["lauthor"] . "</td>";
-	  echo "<td>". $booksrow["isbn"] . "</td>";
+	  echo "<td>". $booksrow["ISBN"] . "</td>";
 	  echo "<td><input type=\"submit\" value=\"Return\" name=\"Button[". $count . "]\"</td></tr>";
 	  $count++;
 	}
@@ -87,27 +87,27 @@ $result = $conn->query($sql);
         //deletion. This if knows what "return" button was pushed.
 	if($_POST['Button']){
 	  $rowIndexToDelete = key($_POST['Button']);
-	  $isbnToDelete; 
+	  $ISBNToDelete; 
 	  $titleToDelete;
           //I know which row in the associative array I want but I dont know how to
           //index that row directly so what happens here is that we iterate through
           //the whole table again but this time we have an index for the row we want
-          //to delete and we catch with an if to collect the isbn. 
-	  $sql = "SELECT * FROM reserves where libid = " . $currentUserID ;
+          //to delete and we catch with an if to collect the ISBN. 
+	  $sql = "SELECT * FROM reserves where libID = " . $currentUserID ;
 	  $result = $conn->query($sql); 
 	  $SecondIterationCount = 0;
 	  while($row = $result->fetch_assoc()){
-	    $returnUsersBooksSQL = "SELECT * FROM books where isbn = " . $row["isbn"];
+	    $returnUsersBooksSQL = "SELECT * FROM books where ISBN = " . $row["ISBN"];
 	    $returnUsersResult = $conn->query($returnUsersBooksSQL);
 	    $booksrow = $returnUsersResult->fetch_assoc();
 	    if($SecondIterationCount == $rowIndexToDelete){
 		$titleToDelete = $booksrow["title"];
-		$isbnToDelete = $booksrow["isbn"];
+		$ISBNToDelete = $booksrow["ISBN"];
 	    }
 	    $SecondIterationCount++;
 	  }
           //Finaly we delete that stinky row. 
-	  $delete_sql = "DELETE FROM reserves WHERE isbn = " . $isbnToDelete;
+	  $delete_sql = "DELETE FROM reserves WHERE ISBN = " . $ISBNToDelete;
 	  if($conn->query($delete_sql) === TRUE){
 	    echo "Give me a sec while I return " . $titleToDelete;	
 	    echo '<meta http-equiv="refresh" content="3"/>';
