150ca0f6dd896f5b3a23702be91b0775c6a938c0
shannonolds <43017295+shannonolds@users.noreply.github.com>
2018-12-06 16:55:02 -0800

    Delete return.php

--- return.php
+++
@@ -1,6 +0,0 @@
-In return <br><br>
-Testing <br><br>
-<a href="loggedin.php">Back</a>
-
-<!-- This file will allow people to return books that they have checked out. 
-It will do this by deleting the book from the reserves table. -->
